d8f0dc008422925068f9f7d90d72ed2190123fb8
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-05-20 12:02:29 +0300

    converted the app to use function components and useState to update the state

--- src/App.js
+++ src/App.js
@@ -1,68 +1,119 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
-class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      monsters: [],
-      searchText: "",
-    };
+const App = () => {
 
-    // binding the function  to the context of the this keyword of the constructor
-    // this.handleChange = this.handleChange.bind(this)
-  }
+  const [monsters, setMonsters] = useState([])
+  const [searchText, setSearchText] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+      .then(users => setMonsters(users))
+  }, [monsters, searchText])
 
 
+  const handleChange = (e) => {
+    const searchField = e.target.value.toLowerCase();
+    setSearchText(searchField)
   }
+  {/* //using filter array method to filter out monsters based on the searchText */ }
 
+  const filteredMonster = monsters.filter((monster) => monster.name.toLowerCase().includes(searchText))
 
-  // handleChange(e) {
-  //   this.setState({ searchText: e.target.value })
-  // }
-
-
-  //arrow functions dont have their own this keyword so its bound to the this context fo the place its defined
-  handleChange = (e) => {
-    this.setState({ searchText: e.target.value })
-  }
-  render() {
-
-    //destructuring the monster and searchText state
-    const { monsters, searchText } = this.state;
+  // const filteredMonster = monsters.filter((monster) => monster.name.toLowerCase().includes(searchText.toLowerCase())
 
 
 
-    //using filter array method to filter out monsters based on the searchText
-    const filteredMonster = monsters.filter((monster) => monster.name.toLowerCase().includes(searchText.toLowerCase())
 
-    )
-    return (
-      <div className='App'>
-        {/* passing the state as a prop */}
 
-        <h1>
-          Monsters App
-        </h1>
+  return (
+    <div className='App'>
 
-        <SearchBox
-          placeholder='search monsters'
-          handleChange={this.handleChange}
-        />
+      <h1>
+        Monsters App
+      </h1 >
 
-        <CardList monsters={filteredMonster} />
+      <SearchBox
+        placeholder='search monsters'
+        onChangeHandler={handleChange}
+      />
 
-      </div>
-    );
-  }
+      <CardList monsters={filteredMonster} />
+
+    </div >
+
+
+  );
+
+
+
 }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       monsters: [],
+//       searchText: "",
+//     };
+
+//     // binding the function  to the context of the this keyword of the constructor
+//     // this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(users => this.setState({ monsters: users }))
+
+
+//   }
+
+
+//   // handleChange(e) {
+//   //   this.setState({ searchText: e.target.value }) 
+//   // }
+
+
+//   //arrow functions dont have their own this keyword so its bound to the this context fo the place its defined
+//   handleChange = (e) => {
+//     setSearchText({ searchText: e.target.value })
+//     // this.setState({ searchText: e.target.value })
+//   }
+//   render() {
+
+//     //destructuring the monster and searchText state
+//     const { monsters, searchText } = this.state;
+
+
+//     const { handleChange } = this;
+
+//     //using filter array method to filter out monsters based on the searchText
+//     const filteredMonster = monsters.filter((monster) => monster.name.toLowerCase().includes(searchText.toLowerCase())
+
+//     )
+//     return (
+//       <div className='App'>
+//         {/* passing the state as a prop */}
+
+//         <h1>
+//           Monsters App
+//         </h1>
+
+//         <SearchBox
+//           placeholder='search monsters'
+//           onChangeHandler={handleChange}
+//         />
+
+//         <CardList monsters={filteredMonster} />
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import './search-box.styles.css'
-const SearchBox = ({ placeholder, handleChange }) => {
+const SearchBox = ({ placeholder, onChangeHandler }) => {
     return (
         <div >
             <input className='search' type="search" placeholder={placeholder}
-                onChange={handleChange} />
+                onChange={onChangeHandler} />
 
         </div>
     )
